eb1a30a134c889b76e0b439f5b4d2c93c9411750
Christophe Fauchard <christophe.fauchard@gmail.com>
2016-05-14 00:23:03 +0200

    cryptage simple par masquage termine

--- crypto.py
+++ crypto.py
@@ -9,30 +9,44 @@
 #-----------------------------------------------------------------
 
 import os
-import base64
 
 class SimpleCrypt():
 
     def __init__(self, key):
         self.key = key
-        self.decoded_text = ''
-        self.encoded_text = ''
+        self.byte_key = bytearray(self.key.encode("latin1"))
     
     def encrypt(self, decoded_text):
-    
-        self.decoded_text = bytearray(decoded_text.encode("utf8"))
+        self.decoded_text = decoded_text
+        self.byte_decoded_text = bytearray(self.decoded_text.encode("latin1"))
+        self.byte_encoded_text = bytearray(self.decoded_text.encode("latin1"))        
+                
+        j = 0
+        for i in range(0, len(self.byte_decoded_text)):
+            if j == len(self.byte_key):
+                j = 0
+            self.byte_encoded_text[i] = self.byte_decoded_text[i] + self.byte_key[j]
+            j += 1
         
-        print(type(decoded_text))
-        print(decoded_text)
-        print(decoded_text[0])
-        print(self.key[0])
-        decoded_text[0] += self.key[0]
-        print(decoded_text[0])
-    
+        self.encoded_text = self.byte_encoded_text.decode("latin1")
+ 
+    def decrypt(self, encoded_text):
+        self.encoded_text = encoded_text
+        self.byte_decoded_text = bytearray(self.encoded_text.encode("latin1"))
+        self.byte_encoded_text = bytearray(self.encoded_text.encode("latin1"))
+                
+        j = 0
+        for i in range(0, len(self.byte_encoded_text)):
+            if j == len(self.byte_key):
+                j = 0
+            self.byte_decoded_text[i] = self.byte_encoded_text[i] - self.byte_key[j]
+            j += 1
+        
+        self.decoded_text = self.byte_decoded_text.decode("latin1")
+            
     def __str__(self):
         return_string = "decoded text: " + self.decoded_text + "\nencoded text: " + self.encoded_text
         return(return_string)
-
         
 if __name__ == '__main__':
     import sys
@@ -43,8 +57,10 @@ if __name__ == '__main__':
     print("Running tests for cryto.py...")
 
     print("testing class SimpleCrypt...")
-    cypher = SimpleCrypt(bytearray("key".encode("utf8")))
+    cypher = SimpleCrypt("fghnfvcghklmbd45689JGHB")
     cypher.encrypt("message secret")
+    print(cypher)
+    cypher.decrypt(cypher.encoded_text)
     print(cypher)
     print("Class SimpleCrypt passed")
 
