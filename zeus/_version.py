ddc52b05777034e3977f94495f639b7b922c6dbf
Christophe Fauchard <christophe.fauchard@gmail.com>
2016-06-06 18:01:46 +0200

    - version 3.1.0.b3

--- zeus/_version.py
+++ zeus/_version.py
@@ -6,6 +6,6 @@
 # Copyright (C) 2016, Christophe Fauchard
 #-----------------------------------------------------------------
 
-__version_info__ = (3, 1, 0, "b2")
+__version_info__ = (3, 1, 0, "b3")
 
 __version__ = '.'.join(map(str, __version_info__))
